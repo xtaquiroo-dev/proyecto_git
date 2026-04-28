b99d217e15d3e2bd074b8e7496e5dc95d1f77283
xtaquiroo-dev <xtaquiroo@gmail.com>
2026-04-27 22:10:05 -0600

    Se integra main.py con llamadas a funciones

---
+++ main.py
@@ -0,0 +1,9 @@
+import funcion_n
+
+def main():
+    print(funcion_n.suma(5, 3))
+    print(funcion_n.resta(5, 3))
+    print(funcion_n.multiplicacion(5, 3))
+
+if __name__ == "__main__":
+    main()
